67413578d9c1a353d3b180f2a9ce23c681dd95bf
Oana Berentan <oana.berentan@yahoo.com>
2019-12-28 18:39:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,28 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/newteam' do
+<<<<<<< HEAD
      erb :newteam
   end
 
    post '/team' do
      erb :team
    end
+=======
+   erb :newteam
+  end
+
+  post '/team' do
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @c = params["c"]
+
+    erb :team
+  end
+>>>>>>> 4df55a9f7ed4ddfdd05eb99a6a83b1c597530606
 
 end
